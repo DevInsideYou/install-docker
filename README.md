fde5d45399edfe1a19fea6e92c092bfdc323fdef
Vladyslav Pekker <agilesteel@gmail.com>
2019-09-18 21:24:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installs [docker](https://www.docker.com/) on [Ubuntu](https://www.ubuntu.com/)
+
+```bash
+wget -q https://script.install.devinsideyou.com/docker
+sudo chmod +x docker && ./docker
+```
---
+++ install-docker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sudo apt update
+
+# install prerequisites
+sudo apt install -yqqq apt-transport-https ca-certificates curl software-properties-common
+
+# add the GPG key for the official Docker repository
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# add the Docker repository to APT sources
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+# apt-update runs automatically after add-apt-repository since Ubuntu 18
+
+# install docker
+sudo apt install docker-ce
+
+# add current user to docker
+sudo usermod -aG docker ${USER}
+
+# echo docker version
+echo
+sudo docker -v
+echo
+echo '"docker" is now on the path'
+
+# remove yourself
+rm $0
+
+# apply the group modification (this has to be the last thing in the script)
+echo
+echo "Switching current user to yourself: ${USER} to apply the group modification (Current user: ${USER} was added to the docker group so that you can run docker without sudo). If you enter the wrong password simply run 'su -u ${USER}' without the single quotes."
+
+su - ${USER}
